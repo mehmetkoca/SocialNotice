67fd2ab943720f1844943e7ef18d01fab5928746
Mehmet Koca <mehmetkoca@outlook.com>
2017-08-11 17:40:26 +0300

    Completed creating Firebase DB users

--- SignInVC.swift
+++ SignInVC.swift
@@ -10,6 +10,7 @@ import UIKit
 import Firebase
 import FirebaseAuth
 import SwiftKeychainWrapper
+
 class SignInVC: UIViewController {
     
     @IBOutlet weak var emailField: UITextField!
@@ -36,7 +37,8 @@ class SignInVC: UIViewController {
             } else {
                 print("MSG: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": user.providerID]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
             }
         })
@@ -49,7 +51,8 @@ class SignInVC: UIViewController {
                 if error == nil {
                     print("MSG: Email user authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                     
                 } else {
@@ -59,7 +62,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("MSG: Successfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                         }
                     })
@@ -68,7 +72,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("MSG: Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
